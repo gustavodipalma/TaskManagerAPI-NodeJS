e57c5c09d79f7cfd5d952811c023865cff03d57b
Gustavo Di Palma <gustavodipalma@gmail.com>
2025-08-21 17:04:26 -0300

    removing swagger dark theme, was creating a bug

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -5,8 +5,5 @@ import path from 'path';
 const swaggerDocument = YAML.load(path.join(__dirname, 'api.yaml'));
 
 export const swaggerSetup = (app: any) => {
-  const options = {
-    customCssUrl: 'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.1/themes/v3/dark.css'
-  };
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
